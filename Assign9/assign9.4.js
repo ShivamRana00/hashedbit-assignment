88ec73e89b4daf434de8807d0aaf3ffb21df549a
ShivamRana00 <shivamkumarrana00@gmail.com>
2025-07-26 07:54:40 +0530

    Delete Assign9 directory

--- Assign9/assign9.4.js
+++
@@ -1,8 +0,0 @@
-// Create an array of numbers and write a function that uses the map() method to return a new array containing each number squared.
-const numbers = [1, 2, 3, 4, 5];
-
-function squareNumbers(arr) {
-  return arr.map(num => num * num);
-}
-
-console.log(squareNumbers(numbers)); // [1, 4, 9, 16, 25]
--- Assign9/assign9.7.js
+++
@@ -1,6 +0,0 @@
-// Write a function that takes an object with properties width and height and returns the area of a rectangle (width * height).
-function getArea(rect) {
-  return rect.width * rect.height;
-}
-
-console.log(getArea({ width: 5, height: 10 })); // 50
