df89770041f2fa185ae7cbe271c41c2317172045
ShivamRana00 <shivamkumarrana00@gmail.com>
2025-07-26 07:44:30 +0530

    Delete Assign3 directory

--- Assign3/assign3.3.js
+++
@@ -1,9 +0,0 @@
-// Q3) let string = 'INDIA'
-// output = 'INDONESIA'
-// Use array.splice
-let arr = ['I', 'N', 'D', 'I', 'A'];
-
-// Replace from index 3: remove 2 items and add the rest of 'ONESIA'
-arr.splice(3, 2, 'O', 'N', 'E', 'S', 'I', 'A');
-
-console.log(arr.join(''));
--- Assign3/assign3.9.js
+++
@@ -1,9 +0,0 @@
-//  Write a function to count the number of words in a paragraph.
-function countWords(paragraph) {
-  let words = paragraph.trim().split(/\s+/);
-  return words.length;
-}
-
-// Example:
-let para = "India is my beautiful country. I love it.";
-console.log(countWords(para)); // Output: 8
